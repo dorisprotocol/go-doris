37f3986bacd78b0a3dc5db173052c1ffc892f36a
lishufan123 <li_shufan@hotmail.com>
2023-11-03 22:55:50 +0800

    doris

--- handlers/static/js/navbar.js
+++ handlers/static/js/navbar.js
@@ -16,30 +16,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-const logOutEl = document.getElementById("navbar-log-out");
-if (logOutEl) {
-  logOutEl.addEventListener("click", () => {
-    logOut().then(() => {
-      document.location = "/";
-    });
-  });
-}
-function setRem() {
-  let designSize = 1920; // 设计图尺寸
-
-  let html = document.documentElement;
-
-  let wW = html.clientWidth;
-  let rem;
-  if (wW > 640) {
-    rem = (wW * 100) / designSize;
-  } else {
-    rem = (wW * 30) / wW;
-  }
-
-  document.documentElement.style.fontSize = rem + "px";
-}
-
-window.addEventListener("load", setRem);
-window.addEventListener("resize", setRem);

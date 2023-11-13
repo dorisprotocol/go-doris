5ba9728b93b7d9a9cdeecb43e2443f49382f08fe
lishufan123 <li_shufan@hotmail.com>
2023-11-13 23:41:45 +0800

    add doris style

--- handlers/static/js/navbar.js
+++ handlers/static/js/navbar.js
@@ -26,19 +26,3 @@ if (logOutEl) {
   });
 }
 
-function setRem() {
-  let designSize = 1920; // 设计图尺寸
-
-  let html = document.documentElement;
-
-  let wW = html.clientWidth;
-  let rem;
-  if (wW > 640) {
-    rem = (wW * 100) / designSize;
-  } else {
-    rem = (wW * 30) / wW;
-  }
-  document.documentElement.style.fontSize = rem + "px";
-}
-window.addEventListener("load", setRem);
-window.addEventListener("resize", setRem);
